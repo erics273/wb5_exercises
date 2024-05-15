3d54a52aaccb06d933031270667a7f58d9ce50a6
Eric Schwartz <erics273@gmail.com>
2024-05-15 14:14:35 -0400

    one more example

--- eric_demos/workingWithMap.js
+++ eric_demos/workingWithMap.js
@@ -3,10 +3,19 @@
 let kids = [
     { first : "Natalie", last : "Plyers" },
     { first: "Brittany", last: "Ray" },
-    { first: "Zachary", last: "Westly" }
+    { first: "Zachary", last: "Westly"}
   ];
 
 
+  //get a new list of just the first names
+  let arrayOfFirstNames = kids.map((kid) => {
+        return kid.first;
+  })
+
+  console.log(arrayOfFirstNames)
+
+
+  //get a new list of the first and last names combined
   let newArrayOfKidsFullNames = kids.map((kid) => {
 
     return `${kid.first} ${kid.last}`;
@@ -15,6 +24,8 @@ let kids = [
 
   console.log(newArrayOfKidsFullNames)
 
+  //get a new list of an object with a new key (property) of fullName added
+  //Full name combines the first and last properties to get it's value
   let newArrayOfKidsWithFullNames = kids.map((kid) => {
 
     return {...kid, fullName: `${kid.first} ${kid.last}`}
